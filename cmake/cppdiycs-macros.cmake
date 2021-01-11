3f6e26b478c3e451a6eb12b4f984e805977da964
Rick Vogel <vrick1991@googlemail.com>
2021-01-11 19:39:07 +0100

    testing

--- cmake/cppdiycs-macros.cmake
+++ cmake/cppdiycs-macros.cmake
@@ -1,49 +1,64 @@
-find_package(Python REQUIRED COMPONENTS Interpreter)
+if(WIN32)
+    find_program(python_EXECUTABLE NAMES python PATHS $ENV{PATH})
+else()
+    find_program(python_EXECUTABLE NAMES python3 PATHS $ENV{PATH})
+endif()
+
+if(NOT python_EXECUTABLE)
+    message(FATAL_ERROR "python executable not found")
+else()
+    message(STATUS "${python_EXECUTABLE}")
+endif()
+
 find_package(castxml REQUIRED)
 
 function (castxml_compile)
 
-	cmake_parse_arguments(castxml "" "XML" "SOURCES;INCLUDE_DIRS;TYPES" ${ARGN})
+    cmake_parse_arguments(castxml "" "XML" "SOURCES;INCLUDE_DIRS;TYPES" ${ARGN})
 
     # check existence of required arguments
     foreach(castxml_ARG_NAME XML;SOURCES;INCLUDE_DIRS)
         if (NOT castxml_${castxml_ARG_NAME})
             message(FATAL_ERROR "Missing argument '${castxml_ARG_NAME}'")
         endif()
-    endforeach()    
+    endforeach()
+
+    get_filename_component(castxml_C_COMPILER ${CMAKE_C_COMPILER} ABSOLUTE)
+    get_filename_component(castxml_CXX_COMPILER ${CMAKE_CXX_COMPILER} ABSOLUTE)
 
 	# create castxml command
 	list(APPEND castxml_CMD --castxml-output=1)
-	if (WIN32)	
+	if (WIN32)
 		list(APPEND castxml_CMD --castxml-cc-msvc)
+		list(APPEND castxml_CMD "\"(\"")
+		list(APPEND castxml_CMD ${castxml_CXX_COMPILER})
+		list(APPEND castxml_CMD /std:c++14)
+ 		list(APPEND castxml_CMD "\")\"")
 	else()
-		# TODO
-	endif()	
-	list(APPEND castxml_CMD "(")
-	if (WIN32)	
-		list(APPEND castxml_CMD "\"C:/Program Files (x86)/Microsoft Visual Studio/2017/Enterprise/VC/Tools/MSVC/14.16.27023/bin/Hostx64/x64/cl.exe\"") # TODO get compiler executable from CMake
-	else()
-		# TODO
+		list(APPEND castxml_CMD --castxml-cc-gnu)
+		list(APPEND castxml_CMD "\"(\"")
+		list(APPEND castxml_CMD ${castxml_CXX_COMPILER})
+                list(APPEND castxml_CMD -std=c++14)
+		list(APPEND castxml_CMD "\")\"")
 	endif()
-	list(APPEND castxml_CMD -std=c++14)
-	list(APPEND castxml_CMD ")")
+
 	list(APPEND castxml_CMD -o)
-    list(APPEND castxml_CMD ${castxml_XML})
-    
-    # optional types argument   
-    if(castxml_TYPES)   
+	list(APPEND castxml_CMD ${castxml_XML})
+
+    # optional types argument
+    if(castxml_TYPES)
         string (REGEX REPLACE ";" "," castxml_TYPES "${castxml_TYPES}")
         list(APPEND castxml_CMD --castxml-start)
         list(APPEND castxml_CMD ${castxml_TYPES})
-    endif()	
-	
+    endif()
+
 	# include directories
 	foreach(castxml_INCLUDE_DIR ${castxml_INCLUDE_DIRS})
 		get_filename_component(castxml_INCLUDE_DIR ${castxml_INCLUDE_DIR} ABSOLUTE)
 		list(APPEND castxml_CMD -I)
 		list(APPEND castxml_CMD ${castxml_INCLUDE_DIR})
-	endforeach()	
-	
+	endforeach()
+
 	# input sources
 	list(APPEND castxml_CMD -x)
 	list(APPEND castxml_CMD c++)
@@ -51,11 +66,14 @@ function (castxml_compile)
 		get_filename_component(castxml_SOURCE ${castxml_SOURCE} ABSOLUTE)
 		list(APPEND castxml_CMD ${castxml_SOURCE})
 	endforeach()
-	
+
 	# disable warnings
 	list(APPEND castxml_CMD -Wno-pragma-once-outside-header)
 
-	# compile the sources into an xml 
+	string(REPLACE ";" " " castxml_CMD "${castxml_CMD}")
+	message(STATUS "${castxml_CMD}")
+
+	# compile the sources into an xml
 	add_custom_command(OUTPUT ${castxml_XML}
 					   COMMAND $<TARGET_FILE:castxml> ${castxml_CMD}
 					   DEPENDS ${castxml_SOURCES}
@@ -92,7 +110,7 @@ function (python_transform)
     # transform the xml into c++ code
     add_custom_command(OUTPUT ${python_OUTPUTS}
                        COMMAND set PYTHONPATH=${python_UTIL_SCRIPT_DIR}
-                       COMMAND $<TARGET_FILE:Python::Interpreter> ${Python_CMD}
+                       COMMAND ${python_EXECUTABLE} ${Python_CMD}
                        DEPENDS ${python_INPUTS}
                        USES_TERMINAL)
 endfunction()
